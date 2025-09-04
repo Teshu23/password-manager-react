4d77ac2622c6d55c1c17acab6a7d22887794ba45
Teshumala Kalihari <priyakalihari2003@gmail.com>
2025-09-04 13:05:21 +0530

    Fix Navbar import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import "../src/App.css"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Manager from "./components/Manager"
 
 export default function App() {
